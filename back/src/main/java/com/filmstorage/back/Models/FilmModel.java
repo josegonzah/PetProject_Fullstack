72a57c95859caab91030657ab222e16f44b2dd78
Davito19 <omardbonett@gmail.com>
2021-11-30 09:09:09 -0500

    Agregando el modelo de filme

---
+++ back/src/main/java/com/filmstorage/back/Models/FilmModel.java
@@ -0,0 +1,46 @@
+package com.filmstorage.back.Models;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class FilmModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(unique = true, nullable = false)
+    private Long id;
+    private String name;
+    private String genre;
+    private Integer duration;
+    
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getGenre() {
+        return genre;
+    }
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+    public Integer getDuration() {
+        return duration;
+    }
+    public void setDuration(Integer duration) {
+        this.duration = duration;
+    }
+    
+    
+}
+
