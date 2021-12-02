412009181b1dc68bcd4e8f6672c457bf5c6ef00d
Davito19 <omardbonett@gmail.com>
2021-12-01 19:03:21 -0500

    Agregando atributos de descripcion

--- back/src/main/java/com/filmstorage/back/Models/FilmModel.java
+++ back/src/main/java/com/filmstorage/back/Models/FilmModel.java
@@ -14,10 +14,17 @@ public class FilmModel {
     private Long id;
     private String name;
     private String genre;
+    private String description;
     private Integer duration;
     
     public Long getId() {
         return id;
+    }
+    public String getDescription() {
+        return description;
+    }
+    public void setDescription(String description) {
+        this.description = description;
     }
     public void setId(Long id) {
         this.id = id;
