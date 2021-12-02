d1666053aec091c91c22a89399314dfeeafdde93
Davito19 <omardbonett@gmail.com>
2021-12-01 20:08:11 -0500

    Agregando las funciones de busqueda por genre y name

--- back/src/main/java/com/filmstorage/back/Repositories/FilmRepository.java
+++ back/src/main/java/com/filmstorage/back/Repositories/FilmRepository.java
@@ -1,9 +1,13 @@
 package com.filmstorage.back.Repositories;
 
+import java.util.ArrayList;
+import java.util.Optional;
+
 import com.filmstorage.back.Models.FilmModel;
 
 import org.springframework.data.repository.CrudRepository;
 
 public interface FilmRepository extends CrudRepository<FilmModel, Long>{
-    
+    public abstract Optional<FilmModel> findByName(String name);
+    public abstract ArrayList<FilmModel> findByGenre(String genre);
 }
